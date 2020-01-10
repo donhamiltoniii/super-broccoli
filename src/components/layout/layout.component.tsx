5cde48814ea4c5ee5512e8d41b20205b71250c72
Don Hamilton III <donhamiltoniii@gmail.com>
2020-01-09 19:43:50 -0500

    Reorganize directory structure

---
+++ src/components/layout/layout.component.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+
+import Header from "../header/header.component";
+import "./layout.styles.scss";
+
+const Layout: React.FC = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </>
+  );
+};
+
+export default Layout;
